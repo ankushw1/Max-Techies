17a4274bdf5f386c7dd2f6fcc3f54ffc937543ac
Ankush Waghmare <waghmareankush050@gmail.com>
2023-05-24 08:00:33 +0530

    adding navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Services from "./Services";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    Max Techies</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
